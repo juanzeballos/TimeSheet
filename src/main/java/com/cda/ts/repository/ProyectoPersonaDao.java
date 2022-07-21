9507ed80369a4ed81d1801477c38b715a256b2be
Juan Bautista Sande <jsande@cdainfo.com>
2022-07-21 14:08:46 -0300

    Se corrigio una referencia erronea a una tabla sin alias en la query de proyectos

--- src/main/java/com/cda/ts/repository/ProyectoPersonaDao.java
+++ src/main/java/com/cda/ts/repository/ProyectoPersonaDao.java
@@ -15,7 +15,7 @@ public interface ProyectoPersonaDao extends JpaRepository<ProyectoPersona, Long>
 	@Query(value = "SELECT PPE_PROYECTO, PRO_DESCRIPCION, PPE_PERSONA,  PPE_TOTAL_HS_REALES, PRO_PERSONA_LIDER, PRO_ABREVIATURA, PRO_EMPRESA_PRINCIPAL \r\n"
 			+ " from PROYECTOS_FASES_PERSONAS inner join PROYECTOS_FASES on PPE_PROYECTO=PFA_PROYECTO and PPE_FASE=PFA_CODIGO inner join ESTADOS_FASES on PFA_ESTADO=ESF_CODIGO inner join ePM_PROYECTOS on PPE_PROYECTO=PRO_CODIGO inner join ESTADOS_PROYECTOS on PRO_ESTADO=ETP_CODIGO inner join PROGRAMAS on PRO_PROGRAMA=PRG_CODIGO inner join PERSONAS_SISTEMAS on PPE_PERSONA=PSI_PERSONA and PSI_SISTEMA=1001 inner join PERSONAS on PSI_PERSONA=PER_CODIGO "
 			+ "  where ETP_UTILIZA_TIMESHEET='S' and ESF_USA_TIMESHEET='S' and PPE_ACTIVO='S' and PSI_USA_TIMESHEET='S'    and  PSI_ACTIVO='S' and PPE_PERSONA  = ?1  "
-			+ "  order by ppe.PPE_PROYECTO DESC",
+			+ "  order by PPE_PROYECTO DESC",
 			nativeQuery = true)
 	public List<Object[]> getProyectosByUsuario(@Param(value = "usuario") String usuario);
 
